21e8ea43079329922cce4017c319b51327630232
chlablak <chlablak@gmail.com>
2017-03-27 13:13:59 -0700

    fsharp start test files

---
+++ trials/fsharp-simple-ebnf/fsharp-simple-ebnf/EBNF.fs
@@ -0,0 +1,16 @@
+﻿module EBNF
+
+open FParsec
+
+type TOperator = 
+      TConcatenation 
+    | TAlternation 
+ 
+type TExpression = 
+      TTerminal of string 
+    | TRule of string 
+    | TBinary of TExpression * TOperator * TExpression 
+ 
+type TStatement = 
+      TDefinition of TExpression * TExpression 
+    | TStatements of TStatement list 
