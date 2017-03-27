15070f30457e1a24ee9986c996ce9feecafededd
chlablak <chlablak@gmail.com>
2017-03-27 08:59:28 -0700

    fsharp simple parser test ok

--- trials/fsharp-simple-ebnf/fsharp-simple-ebnf/EBNF.fs
+++ trials/fsharp-simple-ebnf/fsharp-simple-ebnf/EBNF.fs
@@ -14,3 +14,30 @@ type TExpression =
 type TStatement = 
       TDefinition of TExpression * TExpression 
     | TStatements of TStatement list 
+    
+let rule: Parser<TExpression, unit> = 
+    let alpha = letter <|> digit 
+    let ruleDef = many1Chars2 letter alpha
+    ((pchar '<' >>. ruleDef .>> pchar '>') <|> ruleDef) |>> TRule
+
+let operator: Parser<TOperator, unit> = 
+    let choose c =
+        match c with
+        | ',' -> TConcatenation 
+        | '|' -> TAlternation
+    anyOf ",|" |>> choose 
+
+let expression, expressionRef = createParserForwardedToRef<TExpression, unit> ()
+
+let statements: Parser<TStatement, unit> = 
+    let definition = pipe2 (rule .>> pchar '=') (expression .>> pchar ';') (fun a b -> TDefinition (a, b))
+    many1 definition |>> TStatements
+
+expressionRef := 
+    let terminal = (pchar '\'' >>. many1Chars (noneOf "'") .>> pchar '\'') |>> TTerminal
+    let part = terminal <|> rule
+    let binaryOp = pipe3 part operator expression (fun a b c -> TBinary (a, b, c)) 
+    attempt binaryOp <|> part 
+ 
+
+
--- trials/fsharp-simple-ebnf/fsharp-simple-ebnf/Program.fs
+++ trials/fsharp-simple-ebnf/fsharp-simple-ebnf/Program.fs
@@ -1,4 +1,22 @@
-﻿[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+﻿open FParsec
+open EBNF
+
+[<EntryPoint>]
+let main _ = 
+    let lines = System.IO.File.ReadAllLines("../../../../grammar.ebnf");
+    let text = Array.fold (fun acc x -> acc + x) "" lines 
+    printfn "%s" text
+
+    printfn "%A" (run statements text)
+
+    let timer = System.Diagnostics.Stopwatch ()
+    timer.Start()
+    for i = 0 to 1000000 do
+        let tmp = run statements text
+        0
+    timer.Stop()
+    printfn "%A ms" (float(timer.ElapsedMilliseconds) / 1000000.0)
+
+    printfn "\npress enter to quit..."
+    let _ = System.Console.ReadLine()
+    0 
