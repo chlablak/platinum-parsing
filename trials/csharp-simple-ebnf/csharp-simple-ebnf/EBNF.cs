6bdf521c1ed57998d68660afa4e07ee9de07e8b2
chlablak <chlablak@gmail.com>
2017-03-27 12:03:02 -0700

    csharp simple parser ok

--- trials/csharp-simple-ebnf/csharp-simple-ebnf/EBNF.cs
+++ trials/csharp-simple-ebnf/csharp-simple-ebnf/EBNF.cs
@@ -6,8 +6,39 @@ namespace csharp_simple_ebnf
     {
         public EBNF() : base(false)
         {
-                
-            // this.Root = ;
+            // Terminals
+            IdentifierTerminal TIdentifier = new IdentifierTerminal("identifier");
+            StringLiteral TTerminal = new StringLiteral("terminal", "\'");
+            KeyTerm TConcatenation = ToTerm(",", "concatenation");
+            KeyTerm TAlternation = ToTerm("|", "alternation");
+
+            // Non terminals
+            NonTerminal TOperator = new NonTerminal("operator");
+            NonTerminal TExpression = new NonTerminal("expression");
+            NonTerminal TRule = new NonTerminal("rule");
+            NonTerminal TBinary = new NonTerminal("binary");
+            NonTerminal TStatement = new NonTerminal("statement");
+            NonTerminal TDefinition = new NonTerminal("definition");
+            NonTerminal TStatements = new NonTerminal("statements");
+
+            // Rules definition
+            TOperator.Rule = TConcatenation | TAlternation;
+            TExpression.Rule = TTerminal | TRule | TBinary;
+            TRule.Rule = ToTerm("<") + TIdentifier + ">" | TIdentifier;
+            TBinary.Rule = TExpression + TOperator + TExpression;
+            TStatement.Rule = TDefinition;
+            TDefinition.Rule = TRule + "=" + TExpression + ";";
+            TStatements.Rule = MakePlusRule(TStatements, TStatement);
+
+            // Operators precedence and associativity
+            RegisterOperators(1, TAlternation);
+            RegisterOperators(2, TConcatenation);
+
+            // Clean tree
+            MarkPunctuation("<", ">", "=", ";");
+
+            // Entry rule
+            this.Root = TStatements;
         }
     }
 }
--- trials/csharp-simple-ebnf/csharp-simple-ebnf/Program.cs
+++ trials/csharp-simple-ebnf/csharp-simple-ebnf/Program.cs
@@ -1,12 +1,66 @@
 ﻿using System;
+using Irony.Parsing;
+using System.Diagnostics;
 
 namespace csharp_simple_ebnf
 {
     class Program
     {
+        static public ParseTreeNode getRoot(string sourceCode, Grammar grammar)
+        {
+            LanguageData language = new LanguageData(grammar);
+            Parser parser = new Parser(language);
+            ParseTree parseTree = parser.Parse(sourceCode);
+            ParseTreeNode root = parseTree.Root;
+            return root;
+        }
+
+        static public void dispTree(ParseTreeNode node, int level)
+        {
+            for (int i = 0; i < level; i++)
+                Console.Write("  ");
+            Console.WriteLine(node);
+            foreach (ParseTreeNode child in node.ChildNodes)
+                dispTree(child, level + 1);
+        }
+
+        static public string readFile(string path)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@path);
+            string text = "";
+            foreach (string line in lines)
+                text += line;
+            return text;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                string text = readFile("../../../../grammar.ebnf");
+                Console.WriteLine(text + "\n");
+
+                EBNF g = new EBNF();
+                dispTree(getRoot(text, g), 0);
+
+                Stopwatch timer = new Stopwatch();
+                timer.Start();
+                for (int i = 0; i < 1000000; i++)
+                {
+                    getRoot(text, g);
+                    if (i % 100000 == 0)
+                        Console.WriteLine(i);
+                }
+                timer.Stop();
+                Console.Write("\n" + (timer.ElapsedMilliseconds / 1000000.0) + "ms");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+            Console.WriteLine("\npress enter to quit...");
+            Console.ReadLine();
         }
     }
 }
