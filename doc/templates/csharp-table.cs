35a6c83097f031dc6cec0489f4e386d4ca2f1367
chlablak <chlablak@gmail.com>
2017-06-01 15:03:32 +0200

    csharp template v2

--- doc/templates/csharp-table.cs
+++ doc/templates/csharp-table.cs
@@ -6,57 +6,51 @@
 
 using System;
 using System.Collections.Generic;
-using PP.Engines.Lr;
+using /* INSERT LR ENGINE */;
 
 namespace /* INSERT NAMESPACE */
 {
-  class LrTableImpl : LrTable
-  {
-    public static const char EMPTY = 0;
-    public static const LrAction ERROR = new LrAction(LrAction.Type.Error);
-
-    private Dictionary<Tuple<int, char>, LrAction> actions;
-    private Dictionary<Tuple<int, string>, LrAction> gotos;
-
-    /* ACTION table */
-    public override LrAction action(int state, char term)
+    class LrTableImpl : LrTable
     {
-      return actions.TryGetValue(Tuple.Create(state, term), ERROR);
-    }
+        private LrAction value, error;
+        private Dictionary<Tuple<int, char>, LrAction> actions;
+        private Dictionary<Tuple<int, string>, LrAction> gotos;
 
-    /* GOTO table */
-    public override LrAction action(int state, string nonTerm)
-    {
-      return gotos.TryGetValue(Tuple.Create(state, nonTerm), ERROR);
-    }
+        public char Empty()
+        {
+            return (char)0;
+        }
 
-    public LrTableImpl()
-    {
-      /* Generated from Template */
-      /* ----------------------- */
-      $lr.table.rows:{row|
-        $if(row.isTerm)$
-          $if(row.term.isEmpty)$
-            $if(row.action.isReduce)$
-              actions.Add(Tuple.Create($row.state.id$, EMPTY), new LrAction(LrAction.Type.Reduce, $row.action.reduce.length$, "$row.action.reduce.name$"));
-            $elseif(row.action.isShift)$
-              actions.Add(Tuple.Create($row.state.id$, EMPTY), new LrAction(LrAction.Type.Shift, $row.action.shift$));
-            $endif$
-          $else$
-            $if(row.action.isReduce)$
-              actions.Add(Tuple.Create($row.state.id$, '$row.term.symbol$'), new LrAction(LrAction.Type.Reduce, $row.action.reduce.length$, "$row.action.reduce.name$"));
-            $elseif(row.action.isShift)$
-              actions.Add(Tuple.Create($row.state.id$, '$row.term.symbol$'), new LrAction(LrAction.Type.Shift, $row.action.shift$));
-            $elseif(row.action.isAccept)$
-              actions.Add(Tuple.Create($row.state.id$, '$row.term.symbol$'), new LrAction(LrAction.Type.Accept));
-            $endif$
-          $endif$
-        $else$
-          $if(row.action.isGoto)$
-            gotos.Add(Tuple.Create($row.state.id$, "$row.nonTerm.name$"), new LrAction(LrAction.Type.Goto, $row.action.goto$));
-          $endif$
-        $endif$
-      }$
+        /* ACTION table */
+        public LrAction action(int state, char term)
+        {
+            return actions.TryGetValue(Tuple.Create(state, term), out value) ? value : error;
+        }
+
+        /* GOTO table */
+        public LrAction action(int state, string nonTerm)
+        {
+            return gotos.TryGetValue(Tuple.Create(state, nonTerm), out value) ? value : error;
+        }
+
+        public LrTableImpl()
+        {
+            value = null;
+            error = new LrAction(LrAction.Type.Error);
+            actions = new Dictionary<Tuple<int, char>, LrAction>()
+            {
+                $lr.table.rows:{row|$if(row.isTerm)$$if(row.term.isEmpty)$$if(row.action.isReduce)${ Tuple.Create($row.state.id$, EMPTY), new LrAction(LrAction.Type.Reduce, $row.action.reduce.length$, "$row.action.reduce.name$") },
+                $elseif(row.action.isShift)${ Tuple.Create($row.state.id$, EMPTY), new LrAction(LrAction.Type.Shift, $row.action.shift$) },
+                $endif$$else$$if(row.action.isReduce)${ Tuple.Create($row.state.id$, '$row.term.symbol$'), new LrAction(LrAction.Type.Reduce, $row.action.reduce.length$, "$row.action.reduce.name$") },
+                $elseif(row.action.isShift)${ Tuple.Create($row.state.id$, '$row.term.symbol$'), new LrAction(LrAction.Type.Shift, $row.action.shift$) },
+                $elseif(row.action.isAccept)${ Tuple.Create($row.state.id$, '$row.term.symbol$'), new LrAction(LrAction.Type.Accept) },
+                $endif$$endif$$endif$}$
+            };
+            gotos = new Dictionary<Tuple<int, string>, LrAction>()
+            {
+                $lr.table.rows:{row|$if(!row.isTerm)$$if(row.action.isGoto)${ Tuple.Create($row.state.id$, "$row.nonTerm.name$"), new LrAction(LrAction.Type.Goto, $row.action.goto$) },
+                $endif$$endif$}$
+            };
+        }
     }
-  }
 }
--- trials/csharp-engine/csharp-engine/LrTableImpl.cs
+++ trials/csharp-engine/csharp-engine/LrTableImpl.cs
@@ -6,9 +6,9 @@
 
 using System;
 using System.Collections.Generic;
-using csharp_engine.lr_engine;
+using /* INSERT LR ENGINE */;
 
-namespace csharp_engine
+namespace /* INSERT NAMESPACE */
 {
     class LrTableImpl : LrTable
     {
@@ -43,32 +43,33 @@ namespace csharp_engine
                 { Tuple.Create(0, 'x'), new LrAction(LrAction.Type.Shift, 3) },
                 { Tuple.Create(1, ')'), new LrAction(LrAction.Type.Reduce, 1, "E") },
                 { Tuple.Create(1, '+'), new LrAction(LrAction.Type.Shift, 7) },
-                { Tuple.Create(1, Empty()), new LrAction(LrAction.Type.Reduce, 1, "E") },
+                { Tuple.Create(1, EMPTY), new LrAction(LrAction.Type.Reduce, 1, "E") },
                 { Tuple.Create(2, '('), new LrAction(LrAction.Type.Shift, 2) },
                 { Tuple.Create(2, 'x'), new LrAction(LrAction.Type.Shift, 3) },
                 { Tuple.Create(3, ')'), new LrAction(LrAction.Type.Reduce, 1, "F") },
                 { Tuple.Create(3, '*'), new LrAction(LrAction.Type.Reduce, 1, "F") },
                 { Tuple.Create(3, '+'), new LrAction(LrAction.Type.Reduce, 1, "F") },
-                { Tuple.Create(3, Empty()), new LrAction(LrAction.Type.Reduce, 1, "F") },
-                { Tuple.Create(4, Empty()), new LrAction(LrAction.Type.Reduce, 1, "S") },
+                { Tuple.Create(3, EMPTY), new LrAction(LrAction.Type.Reduce, 1, "F") },
+                { Tuple.Create(4, EMPTY), new LrAction(LrAction.Type.Reduce, 1, "S") },
                 { Tuple.Create(5, ')'), new LrAction(LrAction.Type.Reduce, 1, "T") },
                 { Tuple.Create(5, '*'), new LrAction(LrAction.Type.Shift, 9) },
                 { Tuple.Create(5, '+'), new LrAction(LrAction.Type.Reduce, 1, "T") },
-                { Tuple.Create(5, Empty()), new LrAction(LrAction.Type.Reduce, 1, "T") },
+                { Tuple.Create(5, EMPTY), new LrAction(LrAction.Type.Reduce, 1, "T") },
                 { Tuple.Create(7, '('), new LrAction(LrAction.Type.Shift, 2) },
                 { Tuple.Create(7, 'x'), new LrAction(LrAction.Type.Shift, 3) },
                 { Tuple.Create(8, ')'), new LrAction(LrAction.Type.Shift, 11) },
                 { Tuple.Create(9, '('), new LrAction(LrAction.Type.Shift, 2) },
                 { Tuple.Create(9, 'x'), new LrAction(LrAction.Type.Shift, 3) },
                 { Tuple.Create(10, ')'), new LrAction(LrAction.Type.Reduce, 3, "E") },
-                { Tuple.Create(10, Empty()), new LrAction(LrAction.Type.Reduce, 3, "E") },
+                { Tuple.Create(10, EMPTY), new LrAction(LrAction.Type.Reduce, 3, "E") },
                 { Tuple.Create(11, ')'), new LrAction(LrAction.Type.Reduce, 3, "F") },
                 { Tuple.Create(11, '*'), new LrAction(LrAction.Type.Reduce, 3, "F") },
                 { Tuple.Create(11, '+'), new LrAction(LrAction.Type.Reduce, 3, "F") },
-                { Tuple.Create(11, Empty()), new LrAction(LrAction.Type.Reduce, 3, "F") },
+                { Tuple.Create(11, EMPTY), new LrAction(LrAction.Type.Reduce, 3, "F") },
                 { Tuple.Create(12, ')'), new LrAction(LrAction.Type.Reduce, 3, "T") },
                 { Tuple.Create(12, '+'), new LrAction(LrAction.Type.Reduce, 3, "T") },
-                { Tuple.Create(12, Empty()), new LrAction(LrAction.Type.Reduce, 3, "T") },
+                { Tuple.Create(12, EMPTY), new LrAction(LrAction.Type.Reduce, 3, "T") },
+
             };
             gotos = new Dictionary<Tuple<int, string>, LrAction>()
             {
@@ -84,6 +85,7 @@ namespace csharp_engine
                 { Tuple.Create(7, "T"), new LrAction(LrAction.Type.Goto, 1) },
                 { Tuple.Create(9, "F"), new LrAction(LrAction.Type.Goto, 5) },
                 { Tuple.Create(9, "T"), new LrAction(LrAction.Type.Goto, 12) },
+
             };
         }
     }
