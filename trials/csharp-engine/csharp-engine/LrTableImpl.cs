57dfd08a6aa62c2c02c346093240502f27b67e8e
chlablak <chlablak@gmail.com>
2017-06-02 09:56:26 +0200

    test grammar

--- trials/csharp-engine/csharp-engine/LrTableImpl.cs
+++ trials/csharp-engine/csharp-engine/LrTableImpl.cs
@@ -6,9 +6,9 @@
 
 using System;
 using System.Collections.Generic;
-using csharp_engine.lr_engine;
+using /* INSERT LR ENGINE */;
 
-namespace csharp_engine
+namespace /* INSERT NAMESPACE */
 {
     class LrTableImpl : LrTable
     {
@@ -18,7 +18,7 @@ namespace csharp_engine
 
         public char Empty()
         {
-            return (char)0;
+            return /* INSERT EMPTY SYMBOL */;
         }
 
         /* ACTION table */
@@ -39,52 +39,88 @@ namespace csharp_engine
             error = new LrAction(LrAction.Type.Error);
             actions = new Dictionary<Tuple<int, char>, LrAction>()
             {
-                { Tuple.Create(0, '('), new LrAction(LrAction.Type.Shift, 2) },
-                { Tuple.Create(0, 'x'), new LrAction(LrAction.Type.Shift, 3) },
-                { Tuple.Create(1, ')'), new LrAction(LrAction.Type.Reduce, 1, "E") },
-                { Tuple.Create(1, '+'), new LrAction(LrAction.Type.Shift, 7) },
-                { Tuple.Create(1, Empty()), new LrAction(LrAction.Type.Reduce, 1, "E") },
-                { Tuple.Create(2, '('), new LrAction(LrAction.Type.Shift, 2) },
-                { Tuple.Create(2, 'x'), new LrAction(LrAction.Type.Shift, 3) },
-                { Tuple.Create(3, ')'), new LrAction(LrAction.Type.Reduce, 1, "F") },
-                { Tuple.Create(3, '*'), new LrAction(LrAction.Type.Reduce, 1, "F") },
-                { Tuple.Create(3, '+'), new LrAction(LrAction.Type.Reduce, 1, "F") },
-                { Tuple.Create(3, Empty()), new LrAction(LrAction.Type.Reduce, 1, "F") },
-                { Tuple.Create(4, Empty()), new LrAction(LrAction.Type.Reduce, 1, "S") },
-                { Tuple.Create(5, ')'), new LrAction(LrAction.Type.Reduce, 1, "T") },
-                { Tuple.Create(5, '*'), new LrAction(LrAction.Type.Shift, 9) },
-                { Tuple.Create(5, '+'), new LrAction(LrAction.Type.Reduce, 1, "T") },
-                { Tuple.Create(5, Empty()), new LrAction(LrAction.Type.Reduce, 1, "T") },
-                { Tuple.Create(7, '('), new LrAction(LrAction.Type.Shift, 2) },
-                { Tuple.Create(7, 'x'), new LrAction(LrAction.Type.Shift, 3) },
-                { Tuple.Create(8, ')'), new LrAction(LrAction.Type.Shift, 11) },
-                { Tuple.Create(9, '('), new LrAction(LrAction.Type.Shift, 2) },
-                { Tuple.Create(9, 'x'), new LrAction(LrAction.Type.Shift, 3) },
-                { Tuple.Create(10, ')'), new LrAction(LrAction.Type.Reduce, 3, "E") },
-                { Tuple.Create(10, Empty()), new LrAction(LrAction.Type.Reduce, 3, "E") },
-                { Tuple.Create(11, ')'), new LrAction(LrAction.Type.Reduce, 3, "F") },
-                { Tuple.Create(11, '*'), new LrAction(LrAction.Type.Reduce, 3, "F") },
-                { Tuple.Create(11, '+'), new LrAction(LrAction.Type.Reduce, 3, "F") },
-                { Tuple.Create(11, Empty()), new LrAction(LrAction.Type.Reduce, 3, "F") },
-                { Tuple.Create(12, ')'), new LrAction(LrAction.Type.Reduce, 3, "T") },
-                { Tuple.Create(12, '+'), new LrAction(LrAction.Type.Reduce, 3, "T") },
-                { Tuple.Create(12, Empty()), new LrAction(LrAction.Type.Reduce, 3, "T") },
+                { Tuple.Create(0, '0'), new LrAction(LrAction.Type.Shift, 5) },
+                { Tuple.Create(0, '1'), new LrAction(LrAction.Type.Shift, 6) },
+                { Tuple.Create(0, '2'), new LrAction(LrAction.Type.Shift, 7) },
+                { Tuple.Create(0, '3'), new LrAction(LrAction.Type.Shift, 8) },
+                { Tuple.Create(0, '4'), new LrAction(LrAction.Type.Shift, 9) },
+                { Tuple.Create(0, '5'), new LrAction(LrAction.Type.Shift, 10) },
+                { Tuple.Create(0, '6'), new LrAction(LrAction.Type.Shift, 11) },
+                { Tuple.Create(0, '7'), new LrAction(LrAction.Type.Shift, 12) },
+                { Tuple.Create(0, '8'), new LrAction(LrAction.Type.Shift, 13) },
+                { Tuple.Create(0, '9'), new LrAction(LrAction.Type.Shift, 14) },
+                { Tuple.Create(2, '+'), new LrAction(LrAction.Type.Shift, 15) },
+                { Tuple.Create(2, Empty()), new LrAction(LrAction.Type.Reduce, 1, "expression") },
+                { Tuple.Create(3, '*'), new LrAction(LrAction.Type.Shift, 16) },
+                { Tuple.Create(3, '+'), new LrAction(LrAction.Type.Reduce, 1, "factor") },
+                { Tuple.Create(3, Empty()), new LrAction(LrAction.Type.Reduce, 1, "factor") },
+                { Tuple.Create(4, Empty()), new LrAction(LrAction.Type.Reduce, 1, "statement") },
+                { Tuple.Create(5, '*'), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(5, '+'), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(5, Empty()), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(6, '*'), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(6, '+'), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(6, Empty()), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(7, '*'), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(7, '+'), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(7, Empty()), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(8, '*'), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(8, '+'), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(8, Empty()), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(9, '*'), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(9, '+'), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(9, Empty()), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(10, '*'), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(10, '+'), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(10, Empty()), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(11, '*'), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(11, '+'), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(11, Empty()), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(12, '*'), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(12, '+'), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(12, Empty()), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(13, '*'), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(13, '+'), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(13, Empty()), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(14, '*'), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(14, '+'), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(14, Empty()), new LrAction(LrAction.Type.Reduce, 1, "term") },
+                { Tuple.Create(15, '0'), new LrAction(LrAction.Type.Shift, 5) },
+                { Tuple.Create(15, '1'), new LrAction(LrAction.Type.Shift, 6) },
+                { Tuple.Create(15, '2'), new LrAction(LrAction.Type.Shift, 7) },
+                { Tuple.Create(15, '3'), new LrAction(LrAction.Type.Shift, 8) },
+                { Tuple.Create(15, '4'), new LrAction(LrAction.Type.Shift, 9) },
+                { Tuple.Create(15, '5'), new LrAction(LrAction.Type.Shift, 10) },
+                { Tuple.Create(15, '6'), new LrAction(LrAction.Type.Shift, 11) },
+                { Tuple.Create(15, '7'), new LrAction(LrAction.Type.Shift, 12) },
+                { Tuple.Create(15, '8'), new LrAction(LrAction.Type.Shift, 13) },
+                { Tuple.Create(15, '9'), new LrAction(LrAction.Type.Shift, 14) },
+                { Tuple.Create(16, '0'), new LrAction(LrAction.Type.Shift, 5) },
+                { Tuple.Create(16, '1'), new LrAction(LrAction.Type.Shift, 6) },
+                { Tuple.Create(16, '2'), new LrAction(LrAction.Type.Shift, 7) },
+                { Tuple.Create(16, '3'), new LrAction(LrAction.Type.Shift, 8) },
+                { Tuple.Create(16, '4'), new LrAction(LrAction.Type.Shift, 9) },
+                { Tuple.Create(16, '5'), new LrAction(LrAction.Type.Shift, 10) },
+                { Tuple.Create(16, '6'), new LrAction(LrAction.Type.Shift, 11) },
+                { Tuple.Create(16, '7'), new LrAction(LrAction.Type.Shift, 12) },
+                { Tuple.Create(16, '8'), new LrAction(LrAction.Type.Shift, 13) },
+                { Tuple.Create(16, '9'), new LrAction(LrAction.Type.Shift, 14) },
+                { Tuple.Create(17, Empty()), new LrAction(LrAction.Type.Reduce, 3, "expression") },
+                { Tuple.Create(18, '+'), new LrAction(LrAction.Type.Reduce, 3, "factor") },
+                { Tuple.Create(18, Empty()), new LrAction(LrAction.Type.Reduce, 3, "factor") },
                 
             };
             gotos = new Dictionary<Tuple<int, string>, LrAction>()
             {
-                { Tuple.Create(0, "E"), new LrAction(LrAction.Type.Goto, 4) },
-                { Tuple.Create(0, "F"), new LrAction(LrAction.Type.Goto, 5) },
-                { Tuple.Create(0, "S"), new LrAction(LrAction.Type.Goto, 6) },
-                { Tuple.Create(0, "T"), new LrAction(LrAction.Type.Goto, 1) },
-                { Tuple.Create(2, "E"), new LrAction(LrAction.Type.Goto, 8) },
-                { Tuple.Create(2, "F"), new LrAction(LrAction.Type.Goto, 5) },
-                { Tuple.Create(2, "T"), new LrAction(LrAction.Type.Goto, 1) },
-                { Tuple.Create(7, "E"), new LrAction(LrAction.Type.Goto, 10) },
-                { Tuple.Create(7, "F"), new LrAction(LrAction.Type.Goto, 5) },
-                { Tuple.Create(7, "T"), new LrAction(LrAction.Type.Goto, 1) },
-                { Tuple.Create(9, "F"), new LrAction(LrAction.Type.Goto, 5) },
-                { Tuple.Create(9, "T"), new LrAction(LrAction.Type.Goto, 12) },
+                { Tuple.Create(0, "expression"), new LrAction(LrAction.Type.Goto, 4) },
+                { Tuple.Create(0, "factor"), new LrAction(LrAction.Type.Goto, 2) },
+                { Tuple.Create(0, "statement"), new LrAction(LrAction.Type.Goto, 1) },
+                { Tuple.Create(0, "term"), new LrAction(LrAction.Type.Goto, 3) },
+                { Tuple.Create(15, "expression"), new LrAction(LrAction.Type.Goto, 17) },
+                { Tuple.Create(15, "factor"), new LrAction(LrAction.Type.Goto, 2) },
+                { Tuple.Create(15, "term"), new LrAction(LrAction.Type.Goto, 3) },
+                { Tuple.Create(16, "factor"), new LrAction(LrAction.Type.Goto, 18) },
+                { Tuple.Create(16, "term"), new LrAction(LrAction.Type.Goto, 3) },
                 
             };
         }
