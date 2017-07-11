3fadf63e6012b6e8a3558ed6a45f4721b07a99f8
chlablak <chlablak@gmail.com>
2017-07-11 10:02:47 +0200

    Work.reuse ok, report ok

--- cli/Work.hs
+++ cli/Work.hs
@@ -27,8 +27,8 @@ import           System.IO
 -- |Work's directory
 wDir :: FilePath -> FilePath
 wDir p = ".pp-work/" ++ p
+wDir2 a b = wDir a ++ "/" ++ b
 wFile = wDir "work.yaml"
-bDir p = wDir "binaries/" ++ p
 
 -- |Initialize module if necessary
 initialize :: Log.Logger
@@ -43,43 +43,83 @@ initialize = do
 
 -- |Reuse previous computation, if any
 reuse :: (Binary a) => Bool -> FilePath -> String -> Log.LoggerIO a -> Log.LoggerIO a
-reuse w p k c = undefined
+reuse w p k c = do
+  Log.pushTag "work"
+  r <-  if w then do
+          m <- modified p k
+          if m then do
+            Log.info $ show (p, k) ++ " modified, use computation and save"
+            computeAndSave p k c
+          else do
+            Log.info $ show (p, k) ++ " not modified"
+            e <- exists' p k
+            if e then do
+              Log.info "use previous work"
+              load p k
+            else do
+              Log.info "no previous work, use computation and save"
+              computeAndSave p k c
+        else do
+          Log.info "disabled, use computation"
+          c
+  Log.popTag
+  return r
+  where
+    computeAndSave p k c = do
+      v <- c
+      save p k v
+      register p k
+      return v
 
 -- |Save a binary file
-save :: (Binary a) => FilePath -> a -> Log.Logger
-save p v = Log.io $ encodeFile p v
+save :: (Binary a) => FilePath -> String -> a -> Log.Logger
+save p k v = do
+  (n, _) <- getHash p
+  Log.io $ createDirectoryIfMissing False (wDir n)
+  Log.io $ encodeFile (wDir2 n k) v
 
 -- |Load a binary file
-load :: (Binary a) => FilePath -> Log.LoggerIO a
-load p = Log.io $ decodeFile p
+load :: (Binary a) => FilePath -> String -> Log.LoggerIO a
+load p k = do
+  (n, _) <- getHash p
+  Log.io $ decodeFile $ wDir2 n k
 
 -- |Check if a file exists
 exists :: FilePath -> Log.LoggerIO Bool
 exists p = Log.io $ doesPathExist p
+exists' p k = do
+  (n, _) <- getHash p
+  exists $ wDir2 n k
 
 -- |Register a file state
-register :: FilePath -> Log.Logger
-register p = do
-  Config cf <- getConfig
-  h <- getHash p
-  let c = Config $ ConfigFile p h : filter (\f -> filepath f /= p) cf
-  setConfig c
+register :: FilePath -> String -> Log.Logger
+register p k = do
+  (n, v) <- getHash p
+  c <- getConfig
+  setConfig $ case filter (\f -> fileid f == n) (files c) of
+    [] -> Config $ ConfigFile p n [ConfigHash k v] : files c
+    [f] -> Config $ ConfigFile p n
+      (ConfigHash k v : filter (\h -> hashname h /= k) (filehash f))
+        : filter (\f -> fileid f /= n) (files c)
 
 -- |Check if a file has changed
-modified :: FilePath -> Log.LoggerIO Bool
-modified p = do
-  Config cf <- getConfig
-  case filter (\f -> filepath f == p) cf of
-    []               -> return True
-    [ConfigFile _ h] -> do
-      h2 <- getHash p
-      return $ h /= h2
+modified :: FilePath -> String -> Log.LoggerIO Bool
+modified p k = do
+  (n, v) <- getHash p
+  c <- getConfig
+  case filter (\f -> fileid f == n) (files c) of
+    [] -> return True
+    [f] -> case filter (\h -> hashname h == k) (filehash f) of
+      []  -> return True
+      [h] -> return $ hashvalue h /= v
 
 -- |Get the hash of a file
-getHash :: FilePath -> Log.LoggerIO T.Text
+getHash :: FilePath -> Log.LoggerIO (String, String)
 getHash p = do
   f <- Log.io $ readFile p
-  return $ T.pack $ show $ asWord64 $ hash f
+  return (hash' p, hash' f)
+  where
+    hash' = show . asWord64 . hash
 
 -- |Working file structure
 newtype Config = Config
@@ -87,7 +127,12 @@ newtype Config = Config
   } deriving (Eq, Show)
 data ConfigFile = ConfigFile
   { filepath :: FilePath
-  , filehash :: T.Text
+  , fileid   :: String
+  , filehash :: [ConfigHash]
+  } deriving (Eq, Show)
+data ConfigHash = ConfigHash
+  { hashname  :: String
+  , hashvalue :: String
   } deriving (Eq, Show)
 
 instance Y.FromJSON Config where
@@ -97,9 +142,14 @@ instance Y.ToJSON Config where
   toJSON v = Y.object ["files" .= files v]
 
 instance Y.FromJSON ConfigFile where
-  parseJSON (Y.Object v) = ConfigFile <$> v .: "path" <*> v .: "hash"
+  parseJSON (Y.Object v) = ConfigFile <$> v .: "path" <*> v .: "id" <*> v .: "hash"
 instance Y.ToJSON ConfigFile where
-  toJSON v = Y.object ["path" .= filepath v, "hash" .= filehash v]
+  toJSON v = Y.object ["path" .= filepath v, "id" .= fileid v, "hash" .= filehash v]
+
+instance Y.FromJSON ConfigHash where
+  parseJSON (Y.Object v) = ConfigHash <$> v .: "name" <*> v .: "value"
+instance Y.ToJSON ConfigHash where
+  toJSON v = Y.object ["name" .= hashname v, "value" .= hashvalue v]
 
 -- |Get the config
 getConfig :: Log.LoggerIO Config
